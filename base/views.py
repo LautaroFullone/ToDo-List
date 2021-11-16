0820b1a213dd79a969cb0995588446857843857c
Lautaro Fullone <lautarofullone@gmail.com>
2021-11-16 16:10:14 -0300

    best view

--- base/views.py
+++ base/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.db import transaction
+from django.shortcuts import render, redirect
+from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
@@ -9,6 +11,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
+from .forms import PositionForm
 from .models import Task
 
 
@@ -24,27 +27,37 @@ class CustomLoginView(LoginView):
 class RegisterPage(FormView):
     template_name = 'base/register.html'
     form_class = UserCreationForm
-    redirect_authenticated_user = True
+    redirect_authenticated_user = True #no funciona
     success_url = reverse_lazy('tasks')
 
     def form_valid(self, form):
         user = form.save()
-
         if user is not None:
             login(self.request, user)
-
         return super(RegisterPage, self).form_valid(form)
 
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
 
-    # restringe la vista a logged users
+
+                #restringe la vista a logged users
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
-    def get_context_data(self, **kwargs):  # filtra las taks del user logeado
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(complete=False).count()  # nose muy bien que significa el complete=False
+        context['count'] = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__startswith= search_input)
+
+        context['search_input'] = search_input
+
         return context
 
 
@@ -56,18 +69,17 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = ['title', 'description',
-              'complete']  # SON LOS ATRIBUTOS DEL MODELO QUE QUEREMOS EN EL FORMULARIO -> ['title', 'description']
+    fields = ['title', 'description', 'complete']  # SON LOS ATRIBUTOS DEL MODELO QUE QUEREMOS EN EL FORMULARIO -> ['title', 'description']
     success_url = reverse_lazy('tasks')  # cuando es success envio a la vista de tasks
 
-    def form_invalid(self, form):
+    def form_valid(self, form):   #asigna el usuario que crea la Task, al objeto task
         form.instance.user = self.request.user
         return super(TaskCreate, self).form_valid(form)
 
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 
@@ -75,3 +87,19 @@ class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
+
+    def get_queryset(self):
+        owner = self.request.user
+        return self.model.objects.filter(user=owner)
+
+class TaskReorder(View):
+    def post(self, request):
+        form = PositionForm(request.POST)
+
+        if form.is_valid():
+            positionList = form.cleaned_data["position"].split(',')
+
+            with transaction.atomic():
+                self.request.user.set_task_order(positionList)
+
+        return redirect(reverse_lazy('tasks'))
